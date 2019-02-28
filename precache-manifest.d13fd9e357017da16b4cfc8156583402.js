e1268b4c22fb0b6a1e0dc2d793aca2ed0ac42ae6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-28 22:09:38 +0000

    Deploy zero-to-mastery/face-rec-app to github.com/zero-to-mastery/face-rec-app.git:gh-pages

---
+++ precache-manifest.d13fd9e357017da16b4cfc8156583402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3870336871c01d989306",
+    "url": "./static/css/main.b48e6604.chunk.css"
+  },
+  {
+    "revision": "3870336871c01d989306",
+    "url": "./static/js/main.38703368.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "024a8b7601835a92626c",
+    "url": "./static/css/2.45187fac.chunk.css"
+  },
+  {
+    "revision": "024a8b7601835a92626c",
+    "url": "./static/js/2.024a8b76.chunk.js"
+  },
+  {
+    "revision": "ca54c6828e96ad965ea87ae927d3c974",
+    "url": "./index.html"
+  }
+];
